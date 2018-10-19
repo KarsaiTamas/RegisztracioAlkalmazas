d58319fa88e76fc930fde5a28a03e5be74e2f4de
Tamás Karsai <Karsai106@gmail.com>
2018-10-19 15:45:07 +0200

    RegisztracioAlkalmazas kinézete megvan

---
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace RegisztracioAlkalmazas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nev_Label = new System.Windows.Forms.Label();
+            this.Szul_Cim_Label = new System.Windows.Forms.Label();
+            this.Nem_Label = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Kedvenc_Hobbi_Label = new System.Windows.Forms.Label();
+            this.Uj_Hobbi_Label = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Hozzaad_Button = new System.Windows.Forms.Button();
+            this.Betolt_Button = new System.Windows.Forms.Button();
+            this.Ment_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Nev_Label
+            // 
+            this.Nev_Label.AutoSize = true;
+            this.Nev_Label.Location = new System.Drawing.Point(13, 13);
+            this.Nev_Label.Name = "Nev_Label";
+            this.Nev_Label.Size = new System.Drawing.Size(30, 13);
+            this.Nev_Label.TabIndex = 0;
+            this.Nev_Label.Text = "Név:";
+            // 
+            // Szul_Cim_Label
+            // 
+            this.Szul_Cim_Label.AutoSize = true;
+            this.Szul_Cim_Label.Location = new System.Drawing.Point(13, 41);
+            this.Szul_Cim_Label.Name = "Szul_Cim_Label";
+            this.Szul_Cim_Label.Size = new System.Drawing.Size(54, 13);
+            this.Szul_Cim_Label.TabIndex = 1;
+            this.Szul_Cim_Label.Text = "Szül. cím:";
+            // 
+            // Nem_Label
+            // 
+            this.Nem_Label.AutoSize = true;
+            this.Nem_Label.Location = new System.Drawing.Point(13, 74);
+            this.Nem_Label.Name = "Nem_Label";
+            this.Nem_Label.Size = new System.Drawing.Size(32, 13);
+            this.Nem_Label.TabIndex = 2;
+            this.Nem_Label.Text = "Nem:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(94, 74);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "F";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(161, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(33, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "N";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Uszás",
+            "Horgászat",
+            "Futás"});
+            this.listBox1.Location = new System.Drawing.Point(200, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Kedvenc_Hobbi_Label
+            // 
+            this.Kedvenc_Hobbi_Label.AutoSize = true;
+            this.Kedvenc_Hobbi_Label.Location = new System.Drawing.Point(200, 13);
+            this.Kedvenc_Hobbi_Label.Name = "Kedvenc_Hobbi_Label";
+            this.Kedvenc_Hobbi_Label.Size = new System.Drawing.Size(82, 13);
+            this.Kedvenc_Hobbi_Label.TabIndex = 8;
+            this.Kedvenc_Hobbi_Label.Text = "Kedvenc hobbi:";
+            // 
+            // Uj_Hobbi_Label
+            // 
+            this.Uj_Hobbi_Label.AutoSize = true;
+            this.Uj_Hobbi_Label.Location = new System.Drawing.Point(128, 143);
+            this.Uj_Hobbi_Label.Name = "Uj_Hobbi_Label";
+            this.Uj_Hobbi_Label.Size = new System.Drawing.Size(49, 13);
+            this.Uj_Hobbi_Label.TabIndex = 9;
+            this.Uj_Hobbi_Label.Text = "Új hobbi:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(199, 140);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // Hozzaad_Button
+            // 
+            this.Hozzaad_Button.Location = new System.Drawing.Point(245, 177);
+            this.Hozzaad_Button.Name = "Hozzaad_Button";
+            this.Hozzaad_Button.Size = new System.Drawing.Size(75, 23);
+            this.Hozzaad_Button.TabIndex = 11;
+            this.Hozzaad_Button.Text = "Hozzáad";
+            this.Hozzaad_Button.UseVisualStyleBackColor = true;
+            // 
+            // Betolt_Button
+            // 
+            this.Betolt_Button.Location = new System.Drawing.Point(245, 217);
+            this.Betolt_Button.Name = "Betolt_Button";
+            this.Betolt_Button.Size = new System.Drawing.Size(75, 23);
+            this.Betolt_Button.TabIndex = 12;
+            this.Betolt_Button.Text = "Betöltés";
+            this.Betolt_Button.UseVisualStyleBackColor = true;
+            // 
+            // Ment_Button
+            // 
+            this.Ment_Button.Location = new System.Drawing.Point(161, 217);
+            this.Ment_Button.Name = "Ment_Button";
+            this.Ment_Button.Size = new System.Drawing.Size(75, 23);
+            this.Ment_Button.TabIndex = 13;
+            this.Ment_Button.Text = "Mentés";
+            this.Ment_Button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 252);
+            this.Controls.Add(this.Ment_Button);
+            this.Controls.Add(this.Betolt_Button);
+            this.Controls.Add(this.Hozzaad_Button);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Uj_Hobbi_Label);
+            this.Controls.Add(this.Kedvenc_Hobbi_Label);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Nem_Label);
+            this.Controls.Add(this.Szul_Cim_Label);
+            this.Controls.Add(this.Nev_Label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nev_Label;
+        private System.Windows.Forms.Label Szul_Cim_Label;
+        private System.Windows.Forms.Label Nem_Label;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label Kedvenc_Hobbi_Label;
+        private System.Windows.Forms.Label Uj_Hobbi_Label;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Hozzaad_Button;
+        private System.Windows.Forms.Button Betolt_Button;
+        private System.Windows.Forms.Button Ment_Button;
+    }
+}
+
